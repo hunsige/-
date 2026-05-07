14695915055dce8346acf6b92fd9d988ff534bda
hunsige <ynkpqq9vh6@privaterelay.appleid.com>
2026-05-07 09:17:50 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
-// optional firebase init file
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnttxmLqgf4EehpzvOV45tFzXETBtEq3A",
+  authDomain: "elfbarrrrr-523c7.firebaseapp.com",
+  projectId: "elfbarrrrr-523c7",
+  storageBucket: "elfbarrrrr-523c7.firebasestorage.app",
+  messagingSenderId: "1050313489262",
+  appId: "1:1050313489262:web:f7e30d884bc450d9df9f7f",
+  measurementId: "G-GGS0BWSGG4"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
